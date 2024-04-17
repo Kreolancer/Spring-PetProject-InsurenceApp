6ef5585545499d0786ee79fb304d4ca1efc2ebdf
Kreolancer <112823543+Kreolancer@users.noreply.github.com>
2024-04-17 20:37:30 +0300

    Delete student_kalesnik_ivan_v2 directory

--- student_kalesnik_ivan_v2/src/main/java/lv/javaguru/travel/insurance/core/services/cute/cuteStarter.java
+++
@@ -1,8 +0,0 @@
-package lv.javaguru.travel.insurance.core.services.cute;
-
-import lv.javaguru.travel.insurance.core.api.command.TravelCoreCommand;
-import lv.javaguru.travel.insurance.core.api.command.TravelCoreResult;
-
-public interface cuteStarter {
-    TravelCoreResult getAgreement(TravelCoreCommand command);
-}
