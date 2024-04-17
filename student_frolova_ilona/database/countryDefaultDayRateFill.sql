35f59cef2dfa77a74e19d49298617eb4f066e158
Kreolancer <112823543+Kreolancer@users.noreply.github.com>
2024-04-17 20:36:27 +0300

    Delete student_frolova_ilona directory

--- student_frolova_ilona/database/countryDefaultDayRateFill.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `insurance`.`country_default_day_rate` (`country_ic`, `default_day_rate`) VALUES ('LATVIA', '1.00');
-INSERT INTO `insurance`.`country_default_day_rate` (`country_ic`, `default_day_rate`) VALUES ('SPAIN', '2.50');
-INSERT INTO `insurance`.`country_default_day_rate` (`country_ic`, `default_day_rate`) VALUES ('JAPAN', '3.50');
