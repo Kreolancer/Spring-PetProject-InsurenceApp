937170eb1e951ef3b27b5c35af26f2f570042692
Kreolancer <112823543+Kreolancer@users.noreply.github.com>
2024-04-17 20:38:46 +0300

    Delete student_kroshinsky_eugene directory

--- student_kroshinsky_eugene/src/main/java/lv/javaguru/travel/insurance/core/underwriting/TravelPremiumUnderwriting.java
+++
@@ -1,9 +0,0 @@
-package lv.javaguru.travel.insurance.core.underwriting;
-
-import lv.javaguru.travel.insurance.dto.v1.TravelCalculatePremiumRequestV1;
-
-import java.math.BigDecimal;
-
-public interface TravelPremiumUnderwriting {
-    BigDecimal calculatePremium(TravelCalculatePremiumRequestV1 request);
-}
