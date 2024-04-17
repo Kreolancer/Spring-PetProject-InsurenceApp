206fb22df439900db44bd7e7abe7f64341365fd6
Kreolancer <112823543+Kreolancer@users.noreply.github.com>
2024-04-17 20:41:17 +0300

    Delete student_revinskaya_ekaterina_version_2 directory

--- student_revinskaya_ekaterina_version_2/src/main/java/lv/javaguru/travel/insurance/core/services/TravelGetPolicyService.java
+++
@@ -1,8 +0,0 @@
-package lv.javaguru.travel.insurance.core.services;
-
-import lv.javaguru.travel.insurance.core.api.command.internal.TravelGetPolicyCoreCommand;
-import lv.javaguru.travel.insurance.core.api.command.internal.TravelGetPolicyCoreResult;
-
-public interface TravelGetPolicyService {
-    TravelGetPolicyCoreResult getPolicy(TravelGetPolicyCoreCommand command);
-}
--- student_revinskaya_ekaterina_version_2/src/main/java/lv/javaguru/travel/insurance/core/validations/TravelUuidValidator.java
+++
@@ -1,9 +0,0 @@
-package lv.javaguru.travel.insurance.core.validations;
-
-import lv.javaguru.travel.insurance.core.api.dto.ValidationErrorDTO;
-
-import java.util.List;
-
-public interface TravelUuidValidator {
-    public List<ValidationErrorDTO> validate(String uuid);
-}
